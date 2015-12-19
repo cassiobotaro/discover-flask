0c307423a47da0b98137a197da96e0d83b7c234d
cassiobotaro <cassiobotaro@gmail.com>
2015-12-19 18:39:30 -0200

    Episode 3

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import (Flask, render_template, request, redirect, url_for, session,
+                   flash)
+from functools import wraps
 
 app = Flask(__name__)
 
+app.secret_key = 'my precious'
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 
 @app.route('/')
+@login_required
 def home():
-    return "Hello, world"
+    return render_template('index.html')
 
 
 @app.route('/welcome')
@@ -21,9 +37,18 @@ def login():
                 request.form['password'] != 'admin':
             error = 'Invalid credentials. Please try again.'
         else:
+            session['logged_in'] = True
+            flash('You are just logged in!')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out.')
+    return redirect(url_for('welcome'))
 
 if __name__ == '__main__':
     app.run(debug=True)
