336c7cab0b2fc658f022eca2db1520a0a5996c61
cassiobotaro <cassiobotaro@gmail.com>
2015-12-20 22:40:12 -0200

    Episode 11

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ import os
 
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = 'my precious'
+    SECRET_KEY = '\xbf\xa9p;\xaf8~\x88\xe5\xd9\xd8\xe1JB'\
+                 '\xefN\xa0\x8fn\x92n,\x8dD'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
