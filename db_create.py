bdd0b98676ee8b40eb6c22f867410c7d810ab42e
cassiobotaro <cassiobotaro@gmail.com>
2016-01-01 10:53:54 -0200

    added migrations

--- db_create.py
+++ db_create.py
@@ -7,6 +7,8 @@ db.create_all()
 # insert
 db.session.add(BlogPost("Good", "I'm Good."))
 db.session.add(BlogPost("Well", "I'm well."))
+db.session.add(BlogPost("Flask", "discoverflask.com"))
+db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
 
 # commit the changes
 db.session.commit()
