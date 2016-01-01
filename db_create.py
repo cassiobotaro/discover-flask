2696775faf07fef036aeb2ef9700ced6698241e9
cassiobotaro <cassiobotaro@gmail.com>
2016-01-01 21:32:20 -0200

    Episode 20

--- db_create.py
+++ db_create.py
@@ -1,14 +1,15 @@
-from app import db
-from models import BlogPost
+from project import db
+# from models import BlogPost
 
-# create the database and db tables
+# create the database and the db table
 db.create_all()
 
-# insert
-db.session.add(BlogPost("Good", "I'm Good."))
-db.session.add(BlogPost("Well", "I'm well."))
-db.session.add(BlogPost("Flask", "discoverflask.com"))
-db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
+# insert data
+# db.session.add(BlogPost("Good", "I\'m good."))
+# db.session.add(BlogPost("Well", "I\'m well."))
+# db.session.add(BlogPost("Excellent", "I\'m excellent."))
+# db.session.add(BlogPost("Okay", "I\'m okay."))
+# db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
 
 # commit the changes
 db.session.commit()
